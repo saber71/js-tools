01f296f5a849fda8768c200e0f880cd546f91c4e
saber71 <1533357473@qq.com>
2024-07-06 13:24:57 +0800

    fix(external-deps-plugin): 自动排除node模块，修复打包错误

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
-import swc from "unplugin-swc";
-import { defineConfig } from "vite";
-import { resolve } from "path";
-import dtsPlugin from "vite-plugin-dts";
+import { externalDepsPlugin } from "@heraclius/external-deps-plugin"
+import swc from "unplugin-swc"
+import { defineConfig } from "vite"
+import { resolve } from "path"
+import dtsPlugin from "vite-plugin-dts"
 
 export default defineConfig({
-  plugins: [dtsPlugin({ rollupTypes: true }), swc.vite()],
+  plugins: [dtsPlugin({ rollupTypes: true }), swc.vite(), externalDepsPlugin()],
   build: {
-    rollupOptions: {
-      external: [],
-    },
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       fileName: "index",
-      formats: ["es"],
-    },
-  },
-});
+      formats: ["es"]
+    }
+  }
+})
